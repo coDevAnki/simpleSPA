aeff9d70e0c58b30ab874573e73dce52110986af
unknown <impratsbanerjee@gmail.com>
2020-09-18 09:35:52 +0530

    cleanup and view created initial view directories

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,46 +1,48 @@
+import description from "./description.js";
+
 let l = console.log;
+
+
 const navigateTo = (url) => {
-  l("before push", location.href);
   history.pushState(null, null, url);
   router();
-  l("after push", location.href);
 };
 
 const router = async () => {
   const availableRoutes = [
-    { path: "/", view: () => console.log("viewing home") },
-    { path: "/description", view: () => console.log("viewing description") },
-    { path: "/articles", view: () => console.log("viewing articles") },
+  //  { path: "/", view: () => console.log("viewing home") },
+    { path: "/description", view: description },
+  //  { path: "/articles", view: () => console.log("viewing articles") },
   ];
-  //   const potentialMatches = availableRoutes.map((route) => {
-  //     return {
-  //       route,
-  //       isMatch: location.pathname === route.path,
-  //     };
-  //   });
-  //   console.log(potentialMatches);
 
   const currentRoute = availableRoutes.find(
     (route) => route.path === location.pathname
   );
-  currentRoute.view();
-};
-const c = (e) => {
-  e.preventDefault;
-  console.log("popstate happened");
+  
+  let currentView= new currentRoute.view();
+  l(currentView.getHtml())
+  document.body.innerHTML= await currentView.getHtml();
+
 };
 
-const d = (e) => {
-  console.log("hash CHANGED!");
+const makeSpa = () => {
+  document.querySelectorAll("a").forEach((a) =>
+    a.addEventListener("click", (e) => {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    })
+  );
+  l("made spa");
 };
-window.addEventListener("popstate", c);
-window.addEventListener("hashchange", d);
-document.querySelectorAll("a").forEach((a) =>
-  a.addEventListener("click", (e) => {
-    e.preventDefault();
-    let data = e.currentTarget.getAttribute("data-link");
-    // navigateTo(e.target.href);
-    history.pushState({ pid: data }, null, "#" + data);
-    console.log(history.state);
-  })
-);
+
+document.addEventListener("DOMContentLoaded", makeSpa);
+
+
+
+//   const potentialMatches = availableRoutes.map((route) => {
+//     return {
+//       route,
+//       isMatch: location.pathname === route.path,
+//     };
+//   });
+//   console.log(potentialMatches);
